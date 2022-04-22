919bf87bce6d548e3356f04692d3778cc2fa3c1e
benjarojas <benjathaboi@gmail.com>
2022-04-22 16:05:48 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -52,8 +52,6 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
     TreeNode *auxNode= tree->root;
     tree->current = tree->root;
 
-    if(is_equal(tree, key, auxNode->pair->key)) return;
-
     int searchComp;
 
     while(1)
