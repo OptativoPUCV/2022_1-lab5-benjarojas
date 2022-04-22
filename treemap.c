3b0087da1730628cddc91c42d1cce1276facb6d6
benjarojas <benjathaboi@gmail.com>
2022-04-22 12:15:16 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -106,13 +106,10 @@ void removeNode(TreeMap * tree, TreeNode* node) {
     if((node->left == NULL) && (node->right == NULL))
     {
         if(node->parent->right == node)
-        {
             node->parent->right = NULL;
-        }
+
         if(node->parent->left == node)
-        {
             node->parent->left = NULL;
-        }
     }
         
 
